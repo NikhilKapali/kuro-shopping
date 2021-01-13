e6460f1aebc0cedbff36abf4d11367b6ccc637c2
NikhilKapali <nikhilkapali12@gmail.com>
2021-01-13 15:20:01 +0545

    fucked up

---
+++ src/homepage.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './homepage.css';
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">GG</h1>
+                    <span className="subtitle">MF</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">RUSH</h1>
+                    <span className="subtitle">MF</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">NADE</h1>
+                    <span className="subtitle">MF</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">FUCK</h1>
+                    <span className="subtitle">MF</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">MOVE</h1>
+                    <span className="subtitle">MF</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
